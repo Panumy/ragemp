d365f5e08fb81f8847f3ad522240f1cec8c2504d
Tom <nerovani111@gmail.com>
2019-12-18 16:16:41 +0100

    Update callbacks.js

--- server-side/callbacks.js
+++ server-side/callbacks.js
@@ -1,8 +1,18 @@
 function playerEnterVehicleHandler(player, vehicle, seat) {
     // Taxi fare information
-    var fare = player.vehicle.getVariable('fareprice');
-    if (player.vehicle.model == mp.joaat('taxi') && fare != 'null') return player.outputChatBox(`The fare price of this Taxi is $${fare} per mile. Do /acceptfare to accept it.`);
+    if (player.vehicle.model == mp.joaat('taxi') && player.vehicle.getVariable('fareprice') != null) return player.outputChatBox(`The fare price of this Taxi is $${player.vehicle.getVariable('fareprice')} per mile. Do /acceptfare to accept it.`);
+    // ---------------------
+}
+
+function playerExitVehicleHandler(player, vehicle) {
+    // If a passenger leaves the taxi
+    if (vehicle.model == mp.joaat('taxi') && vehicle.getVariable('distancefromfarestart') != 0)
+    {
+        player.outputChatBox(`You have <b style='color: red'>left the taxi</b> vehicle and automatically <b style='color: red'>stopped</b> the fare.`);
+        mp.events.call("TaxiFareStop", vehicle.getVariable('taxidriver'), vehicle); // Stops the taxi timer
+    }
     // ---------------------
 }
 
 mp.events.add("playerEnterVehicle", playerEnterVehicleHandler);
+mp.events.add("playerExitVehicle", playerExitVehicleHandler);
