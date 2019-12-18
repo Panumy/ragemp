71711103a9cc14614cf09dd96cd3489549d9e7d4
Tom <nerovani111@gmail.com>
2019-12-18 16:16:55 +0100

    Update events.js

--- client-side/events.js
+++ client-side/events.js
@@ -1,18 +1,19 @@
+mp.gui.chat.safeMode = false;
+
+var TaxiTimer;
+
 let TaxiFareTimerStart = () => { 
-    driver = mp.players.local;
-    var timer = setInterval(function(){ TaxiFareUpdateRedirect(driver); }, 1000);
-    
-    mp.events.callRemote("TaxiTimerAssign",driver.vehicle, timer);
+    driver = mp.players.local; // Grabs the player id
+    TaxiTimer = setInterval(function(){ TaxiFareUpdateRedirect(driver, driver.vehicle); }, 1000); // Starts a 1 second timer
 };
 
-/*let TaxiFareTimerStop = () => { 
-    var timer = driver.vehicle.getVariable('TaxiTimer');
-    clearInterval(timer);
-};*/
+let TaxiFareTimerStop = () => { 
+    clearInterval(TaxiTimer); //  Stops the taxi timer
+};
 
 mp.events.add('TaxiFareTimerStart', TaxiFareTimerStart);
-//mp.events.add('TaxiFareTimerStop', TaxiFareTimerStop);
+mp.events.add('TaxiFareTimerStop', TaxiFareTimerStop);
 
-function TaxiFareUpdateRedirect (driver){
-    mp.events.callRemote("TaxiFareUpdate", driver);
+function TaxiFareUpdateRedirect (driver, vehicle){
+    mp.events.callRemote("TaxiFareUpdate", driver, vehicle); // Calls a server-side function that updates the fare distance
 }
